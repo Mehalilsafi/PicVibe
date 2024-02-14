50301a49ee2339260a7fa0727f0c6f2601596354
safi <safiou102003@gmail.com>
2024-02-14 07:04:59 -0800

    fix : addPost funtion

--- app/actions/addPost.js
+++ app/actions/addPost.js
@@ -1,8 +1,17 @@
 'use server'
 import React from 'react'
+import {createSupabaseServerClient} from '@/utils/supabase/actions'
+export   async function addPost() {
+    const supabase = await createSupabaseServerClient()
+    const {data ,error}= supabase.from('post').insert([{
 
-export default function addPost() {
-  return (
-    <div>addPost</div>
-  )
+        title: title,
+        description: description,
+        url: photo.url 
+    }])
+    if (postError) {
+        console.error('Error inserting data into Post table:', postError);
+    } else {
+        console.log('New post inserted into Post table:', newPost);
+    }
 }
